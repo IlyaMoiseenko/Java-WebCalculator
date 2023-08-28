690f6f0f227d1693ab57510db70d5a7fc4487ff6
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-08-28 15:12:35 +0300

    add: calculator

---
+++ src/main/java/by/tms/calculator/servlet/CalculatorServlet.java
@@ -0,0 +1,39 @@
+package by.tms.calculator.servlet;
+/*
+@author Ilya Moiseenko 28.08.2023
+*/
+
+import by.tms.calculator.models.Operation;
+import by.tms.calculator.models.User;
+import by.tms.calculator.services.OperationService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/calculator")
+public class CalculatorServlet extends HttpServlet {
+
+    private final OperationService operationService = new OperationService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) getServletContext().getAttribute("user");
+
+        if (user != null) {
+            double num1 = Double.parseDouble(req.getParameter("num1"));
+            double num2 = Double.parseDouble(req.getParameter("num2"));
+            String type = req.getParameter("type");
+
+            Operation operation = new Operation(num1, num2, type, user.getId());
+            Operation result = operationService.calculate(operation);
+
+            resp.getWriter().println(result.getResult());
+        } else {
+            resp.getWriter().println("Please, login or register!");
+        }
+    }
+}
