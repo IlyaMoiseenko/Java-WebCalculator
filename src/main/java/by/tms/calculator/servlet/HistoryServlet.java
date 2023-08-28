93c4c058b9c1861da92fce52d487366d315e0ed9
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-08-28 15:17:29 +0300

    add: history

---
+++ src/main/java/by/tms/calculator/servlet/HistoryServlet.java
@@ -0,0 +1,34 @@
+package by.tms.calculator.servlet;
+/*
+@author Ilya Moiseenko 28.08.2023
+*/
+
+import by.tms.calculator.models.User;
+import by.tms.calculator.services.OperationService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/history")
+public class HistoryServlet extends HttpServlet {
+
+    private final OperationService operationService = new OperationService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) getServletContext().getAttribute("user");
+
+        if (user != null) {
+            List<String> historyByUser = operationService.getHistoryByUser(user);
+
+            resp.getWriter().println(historyByUser);
+        } else {
+            resp.getWriter().println("Please, login or register");
+        }
+    }
+}
