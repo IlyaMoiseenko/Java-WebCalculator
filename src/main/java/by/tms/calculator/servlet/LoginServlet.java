448391cecdcc2390298edb8bc03559022af9ac98
Ilya Moiseenko <ilya.moiseenko.00@yandex.by>
2023-08-28 14:58:46 +0300

    add: login

---
+++ src/main/java/by/tms/calculator/servlet/LoginServlet.java
@@ -0,0 +1,35 @@
+package by.tms.calculator.servlet;
+/*
+@author Ilya Moiseenko 28.08.2023
+*/
+
+import by.tms.calculator.models.User;
+import by.tms.calculator.services.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    private UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        Optional<User> user = userService.logIn(username, password);
+        if (user.isPresent()) {
+            getServletContext().setAttribute("user", user.get());
+            resp.getWriter().println("Login success!");
+        } else {
+            resp.getWriter().println("Invalid user data!");
+        }
+    }
+}
